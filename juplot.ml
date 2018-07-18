831ec56b71582bc1e4ea43500c4d734a5e423475
hennequin <gje.hennequin@gmail.com>
2018-07-18 12:55:35 +0100

    better term defs

--- juplot.ml
+++ juplot.ml
@@ -3,6 +3,9 @@ open Printf
 let draw ?gnuplot ?size ?init (f : (module Gp.Figure) -> unit) =
   let module O = struct
     include Gp.SVG
+    let term =
+      let term = match size with Some s -> { term with size = Some s } | None -> term in
+      { term with other = Some "mouse standalone" }
     let post_action = 
       Some (fun root ->
           let ic = open_in_bin (root ^ ".svg") in
@@ -11,9 +14,6 @@ let draw ?gnuplot ?size ?init (f : (module Gp.Figure) -> unit) =
           close_in ic ;
           ignore (Jupyter_notebook.display ~base64:false "image/svg+xml" data);
           ())
-    let term_opts = match size with
-      | Some (w, h) -> term_opts @ [ sprintf "size %i,%i" w h ]
-      | None -> term_opts
   end in
   let fig = Gp.figure ?gnuplot ?init ~to_file:"/tmp/juplot_tmp" (module O) in
   let module F = (val fig: Gp.Figure) in
---
+++ juplot.mli
@@ -0,0 +1,1 @@
+val draw : ?gnuplot:string -> ?size:int*int -> ?init:string -> ((module Gp.Figure) -> unit) -> unit
