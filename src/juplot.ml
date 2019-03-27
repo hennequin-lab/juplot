c2f18a59f7e6c4eae03937b34fd7f2b839c28e99
hennequin <gje.hennequin@gmail.com>
2019-03-27 17:43:27 +0000

    movies, here we come :party: (we can now specify an object ID)

--- src/juplot.ml
+++ src/juplot.ml
@@ -1,23 +1,27 @@
-let draw ?gnuplot ?(fmt=`png) ?size ?init (f : (module Gp.Figure) -> unit) =
-  let (module Out), mime, base64, opts = match fmt with
-    | `png -> ((module Gp.PNG: Gp.Output), "image/png", true, "enhanced transparent crop")
-    | `svg -> ((module Gp.SVG: Gp.Output), "image/svg+xml", false, "standalone") in
+let draw ?display_id ?gnuplot ?(fmt = `png) ?size ?init (f : (module Gp.Figure) -> unit) =
+  let (module Out), mime, base64, opts =
+    match fmt with
+    | `png -> ((module Gp.PNG : Gp.Output), "image/png", true, "enhanced transparent crop")
+    | `svg -> ((module Gp.SVG : Gp.Output), "image/svg+xml", false, "standalone")
+  in
   let module O = struct
     include Out
-    let term = 
-      let term = match size with Some s -> { term with size = Some s } | None -> term in
-      { term with other = Some opts }
-    let post_action = 
-      Some (fun root ->
+
+    let term =
+      let term = match size with Some s -> {term with size= Some s} | None -> term in
+      {term with other= Some opts}
+
+    let post_action =
+      Some
+        (fun root ->
           let ic = open_in_bin (root ^ file_ext) in
           let n = in_channel_length ic in
           let data = really_input_string ic n in
           close_in ic ;
-          ignore (Jupyter_notebook.display ~base64 mime data);
-          ())
+          ignore (Jupyter_notebook.display ?display_id ~base64 mime data) ;
+          () )
   end in
-  let fig = Gp.figure ?gnuplot ?init ~to_file:Filename.(temp_file "juplot_" ".png") (module O) in
-  let module F = (val fig: Gp.Figure) in
-  f (module F);
+  let fig = Gp.figure ?gnuplot ?init ~to_file:"/tmp/juplot" (module O) in
+  let module F = (val fig : Gp.Figure) in
+  f (module F) ;
   F.draw ()
-
--- src/juplot.mli
+++ src/juplot.mli
@@ -1,1 +1,8 @@
-val draw : ?gnuplot:string -> ?fmt:[ `png | `svg ] -> ?size:int*int -> ?init:string -> ((module Gp.Figure) -> unit) -> unit
+val draw :
+     ?display_id:Jupyter_notebook.display_id
+  -> ?gnuplot:string
+  -> ?fmt:[`png | `svg]
+  -> ?size:int * int
+  -> ?init:string
+  -> ((module Gp.Figure) -> unit)
+  -> unit
